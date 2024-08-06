e4fed2c5c8abe73b07060e12ddc790a896c77dc5
omeros <omer.golan.dev@gmail.com>
2024-08-06 16:11:22 +0300

    remove the connetion string to mongoDB

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-
-//  choose the DB - local or at Atlas Cloud - just change those two setting !
-module.exports = {
-  'dbURL': 'mongodb+srv://omer:1234@cluster0.mhoaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',    // atlas cloud
-    //  'dbURL': 'mongodb://localhost:27017',     // local
-}
--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'dbURL': 'mongodb+srv://omer:1234@cluster0.mhoaj.mongodb.net/storyDB?retryWrites=true&w=majority',              // cloud connection
-}
-
-
-
